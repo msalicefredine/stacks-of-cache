887b0964002a96b1884d6f08f9bdf845d6b9b4c5
Alice Fredine <alice.fredine@gmail.com>
2015-03-14 14:40:45 -0700

    Added super amazeballs icon

---
+++ MyFridge/MyFridge/DatePickerViewController.m
@@ -0,0 +1,39 @@
+//
+//  DatePickerViewController.m
+//  MyFridge
+//
+//  Created by Alice Fredine on 2015-03-14.
+//  Copyright (c) 2015 Alice Fredine. All rights reserved.
+//
+
+#import "DatePickerViewController.h"
+
+@interface DatePickerViewController ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (weak, nonatomic) IBOutlet UIButton *saveButton;
+
+@end
+
+@implementation DatePickerViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
